f084e3fb2410209187d462a4a57fd3e17eb91216
Hypersaiph <mygvs.mh@gmail.com>
2018-07-08 22:44:51 -0400

    types of variables: int, float, str, bool

---
+++ TypesOfVariables.py
@@ -0,0 +1,16 @@
+#integer
+x = 2
+x
+print(type(x))
+#float
+x = 7.7
+print(type(x))
+#string
+s = 'hello'
+print(type(s))
+s = "hello"
+print(type(s))
+#logical
+l = True
+l = False
+print(type(l))
