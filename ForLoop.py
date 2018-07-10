49e9fe54edbec59933a0dc33ac0efd8ba3847641
Hypersaiph <mygvs.mh@gmail.com>
2018-07-09 22:42:25 -0400

    for

---
+++ ForLoop.py
@@ -0,0 +1,8 @@
+for i_the_AI in range(5):
+    print("Leeeeeeeeeeeeeeeeeroooooooooooooooooooooy", i_the_AI)
+
+print(list(range(0,5)))
+
+myList = [10, 100, 1000, 1000]
+for i_the_AI in myList:
+    print(i_the_AI)
