68fc3553b5e7f29397407a355f6a8d0a0931486c
Hypersaiph <mygvs.mh@gmail.com>
2018-07-09 23:36:25 -0400

    lists

---
+++ fundamentals/Lists.py
@@ -0,0 +1,23 @@
+
+list_1 = [5, 139, 478, 34, 10000, 24, 578, 4567]
+
+print(type(list_1))
+
+
+list_2 = ["yolo", 139, 478, False, not False, 24, 578, 4567.99]
+
+print(type(list_2))
+
+list_3 = ['is it o k?', not True, list]
+
+print(type(list_3))
+
+#multiple lists
+y = list(range(8))
+print(y)
+#between
+y = list(range(1,8))
+print(y)
+#with steps
+y = list(range(110,201,10))
+print(y)
