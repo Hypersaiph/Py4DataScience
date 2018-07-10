2212904b2517375b8dd42c3c109d9d9b5566cd26
Hypersaiph <mygvs.mh@gmail.com>
2018-07-09 22:21:41 -0400

    vars

---
+++ WorkingWithVariables.py
@@ -0,0 +1,16 @@
+import math
+
+a = 10
+b = 5
+c = a + b
+d = b / a
+print(c)
+print(d)
+
+c = math.sqrt(a)
+print(round(c))
+
+greeting = "hello"
+name = "gvs"
+message = greeting+" "+name
+print(message)
