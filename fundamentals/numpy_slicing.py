c73abd4fbd7fce0ee07857d9433ec642c7570757
Hypersaiph <mygvs.mh@gmail.com>
2018-07-10 22:35:15 -0400

    numpy array slicing

---
+++ fundamentals/numpy_slicing.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+abc_of_list = ['A','B','C','D','E','F','G','H','I','J']
+
+bread = np.array(abc_of_list)
+# now we'll slice the bread :v, get a knife ready
+print(bread[2:4])
+print(bread)
