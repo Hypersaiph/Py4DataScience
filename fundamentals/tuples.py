df2136c173240a0e29de6e2d25a62a13b42d6518
Hypersaiph <mygvs.mh@gmail.com>
2018-07-10 21:57:48 -0400

    tuples

---
+++ fundamentals/tuples.py
@@ -0,0 +1,14 @@
+# LIST
+wii = ['A','B','C','D','E','F','G','H','I','J']
+
+# slice from 2 to 7, 7 is not inclusive
+print(wii[2:7:1])
+# same result, reversed, with negative step of 1
+print(wii[6:1:-1])
+
+# TUPLES: mutable list
+t1 = (765, 283, 354, 756, 934, 234)
+
+print(t1[0])
+print(t1[2])
+print(t1[4])
