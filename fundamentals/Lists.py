90b6a53f4698c6a8043f54b7a2fa0c43827a2eb0
Hypersaiph <mygvs.mh@gmail.com>
2018-07-10 21:51:00 -0400

    slicing

--- fundamentals/Lists.py
+++ fundamentals/Lists.py
@@ -1,8 +1,6 @@
-
 list_1 = [5, 139, 478, 34, 10000, 24, 578, 4567]
 
 print(type(list_1))
-
 
 list_2 = ["yolo", 139, 478, False, not False, 24, 578, 4567.99]
 
@@ -16,15 +14,14 @@ print(type(list_3))
 y = list(range(8))
 print(y)
 # between
-y = list(range(1,8))
+y = list(range(1, 8))
 print(y)
 # with steps
-y = list(range(110,201,10))
+y = list(range(110, 201, 10))
 print(y)
 
-
 # access to lists
-w = ['a','b','c','d','e','f','g']
+w = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
 print(w[1])
 print(w[2])
 print(w[3])
@@ -35,3 +32,25 @@ print(w[-2])
 print(w[-3])
 w[-3] = ':3'
 print(w)
+
+# slicing
+wii = ['A','B','C','D','E','F','G','H','I','j']
+# everything
+print(wii[:])
+# starting from 2
+print(wii[2:])
+# until 7, 7 is not inclusive
+print(wii[:7])
+# range from 2 to 7, 7 is not inclusive
+print(wii[2:7])
+# range from 2 to 7, 7 is not inclusive
+print(wii[-8:-3])
+
+# [ADVANCED] slicing :D!!
+# slice from 2 to element 9, 9th element is not inclusive
+# getting every two members
+print(wii[2:9:2])
+
+# everything, step by 2
+print(wii[::2])
+
