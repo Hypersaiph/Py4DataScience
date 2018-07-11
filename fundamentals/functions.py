7d6889fd7f98efcc0497bc4bbc40ec9b877b44c4
Hypersaiph <mygvs.mh@gmail.com>
2018-07-10 22:03:43 -0400

    functions

---
+++ fundamentals/functions.py
@@ -0,0 +1,18 @@
+# function list
+oreo = list(range(20,31))
+
+# function print()
+print(oreo)
+
+# len function
+print(len(oreo))
+
+# type function
+print(type(oreo))
+
+# el maxsh, function
+print(max(5, 88, 9))
+
+# el min, function
+print(min(5, 88, 9))
+
