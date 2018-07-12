8c2b034439e1c090880f151734e79e7cdc94cb84
Hypersaiph <mygvs.mh@gmail.com>
2018-07-11 21:22:07 -0400

    dictionaries

---
+++ matrices/dictionaries.py
@@ -0,0 +1,13 @@
+# dictionaries
+
+dic_1 = {
+    'key1': 1,
+    'key2': 2,
+    'key3': 3,
+    'key4': 4,
+    'did_LaFrance_win': True,
+}
+
+print(type(dic_1))
+print(dic_1.__len__())
+print(dic_1['did_LaFrance_win'])
