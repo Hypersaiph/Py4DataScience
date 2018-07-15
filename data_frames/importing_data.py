130572eeccc2025ee1f6d0c73c176b84bd4332a2
Hypersaiph <mygvs.mh@gmail.com>
2018-07-15 00:01:30 -0400

    CSV un python :3

--- data_frames/importing_data.py
+++ data_frames/importing_data.py
@@ -2,10 +2,33 @@ import pandas as pd
 
 stats = pd.read_csv('DemographicData.csv')
 
-print(stats)
+# print(stats)
 
 # method 2
 import os
 
-print(os.getcwd())
-print(os.chdir('/usr/'))
+# print(os.getcwd())
+# print(os.chdir('/usr/'))
+
+# Exploring data
+# 1. Full dataframe
+
+#print(len(stats))
+#print(stats.columns)
+
+#print(len(stats.columns))
+
+# 5. top rows
+#print(stats.head()) # remember the brackets
+
+# 6. bottom rows
+#print(stats.tail(6))
+
+# 7. information on the columns
+
+#print(stats.info()) # as in R
+
+#print(stats.describe())
+
+print(stats.describe().transpose())
+
