c845d42eaad5420bfeb6fe3baf6ba839d9fdaeed
Hypersaiph <mygvs.mh@gmail.com>
2018-07-16 22:00:26 -0400

    too much plots

--- data_frames/rename_columns.py
+++ data_frames/rename_columns.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 stats = pd.read_csv('DemographicData.csv')
 
-print(stats.columns)
+#print(stats.columns)
 stats.columns = ['a', 'b', 'c', 'd', 'e']
-print(stats.head())
+#print(stats.head())
 stats.columns = ['CountryName', 'CountryCode', 'BirthRate', 'InternetUsers', 'IncomeGroup']
 print(stats.head())
--- data_frames/subsetting_dataframes.py
+++ data_frames/subsetting_dataframes.py
@@ -10,20 +10,20 @@ stats = pd.read_csv('DemographicData.csv')
 # P1. Rows, starts at 0
 #print(stats[21:26])
 
-print(stats[:10])
+#print(stats[:10])
 
 # show reverse
-print(stats[::-1])
+#print(stats[::-1])
 
 # 20th row
-print(stats[::20])
+#print(stats[::20])
 
 # P2. Columns
 # Only column
-print(stats['Country Name'])
+#print(stats['Country Name'])
 
 # More column, use a list
-print(stats[['Country Name', 'Birth rate']].head())
+#print(stats[['Country Name', 'Birth rate']].head())
 
 # Quick access
 #stats.columns = ['CountryName', 'CountryCode', 'BirthRate', 'InternetUsers', 'IncomeGroup']
@@ -31,5 +31,5 @@ print(stats[['Country Name', 'Birth rate']].head())
 
 # P3. Com ining the two
 # subset 4:8, another subset's been applied
-print(stats[4:8][['Country Name', 'Birth rate']])
+#print(stats[4:8][['Country Name', 'Birth rate']])
 print(stats[['Country Name', 'Birth rate']][4:8])
